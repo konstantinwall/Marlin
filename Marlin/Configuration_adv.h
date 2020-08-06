b86ec7ea53f1da5f3d16b27376b1e4639ae2cf9a
konstantinwall <34520761+konstantinwall@users.noreply.github.com>
2020-08-06 09:02:03 +0200

    Adjusted load filament lenght to new bowden tube

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2048,7 +2048,7 @@
                                                   //   For direct drive, the full length of the nozzle.
                                                   //   Set to 0 for manual unloading.
   #define FILAMENT_CHANGE_SLOW_LOAD_FEEDRATE   35  //kwa // (mm/s) Slow move when starting load.
-  #define FILAMENT_CHANGE_SLOW_LOAD_LENGTH     350  //kwa // (mm) Slow length, to allow time to insert material.
+  #define FILAMENT_CHANGE_SLOW_LOAD_LENGTH     700  //kwa // (mm) Slow length, to allow time to insert material.
                                                   // 0 to disable start loading and skip to fast load only
   #define FILAMENT_CHANGE_FAST_LOAD_FEEDRATE   3  //kwa // (mm/s) Load filament feedrate. This can be pretty fast.
   #define FILAMENT_CHANGE_FAST_LOAD_ACCEL     25  // (mm/s^2) Lower acceleration may allow a faster feedrate.
